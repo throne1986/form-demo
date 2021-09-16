c41bcfda3c96af50c52e518f3d0ec715ce305bc9
gzellah <jellyforjully088@yahoo.com>
2021-09-16 16:41:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "vid-form";
 
 function App() {
+  console.log("test", Form);
+  const handleSubmit = () => {
+    console.log("cos pieknego");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testing 111111</h1>
+
+      <Form name="Name from videommerce app" handleSubmit={handleSubmit} />
     </div>
   );
 }
